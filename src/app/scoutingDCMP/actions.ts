f14e64e926f847ca03a56c919c1a921bfc4ba4ec
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-03-11 21:26:28 +0200

    scouting with forms added

---
+++ src/app/scoutingDCMP/actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export async function editHttpRequest(data: any) {
+    const response = await fetch('https://MA5951.pythonanywhere.com/update_image', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+  
+    if (response.ok) {
+        return { success: true };
+    } else {
+        return { success: false, error: 'Failed to process the request' };
+    }
+}
