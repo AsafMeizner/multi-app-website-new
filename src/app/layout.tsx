8380ccbf306da27ea02e1ea53a0c26073834362f
Asaf Meizner <75739196+AsafMeizner@users.noreply.github.com>
2024-04-06 19:51:01 +0300

    discord

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const disallowedPaths = ['urlShortner/', 'scouting', '3d', '/.well-known'];
+const disallowedPaths = ['urlShortner/', 'scouting', '3d'];
 
 export default function RootLayout({
   children,
